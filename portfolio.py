936f46618b238e9c8940f0072d2f9351af2684a4
Alonso Sandoval <alonso.sandoval@magnet.cl>
2024-11-17 13:12:41 -0300

    fix: Simplify the implementation of the annualized return.

--- portfolio.py
+++ portfolio.py
@@ -1,78 +1,51 @@
-from datetime import datetime
-import random
+from datetime import date
 
 
 class Stock():
+    """
+    Represent an stock that only have 2 values for test purpose, an "initial value"
+    and a "end value".
+    """
+    def __init__(self):
+        self.switch = 0
+        self.values = [5, 6.187]
+
     def price(self, price_date):
-        # Random float between 10 and 50
-        random_number = random.uniform(10, 50)
-        return random_number
+        price = self.values[self.switch]
+        self.switch = (self.switch + 1) % 2 # Switch between the hardcoded values 
+        return price
 
 class Portfolio():
     """
-    A class that represent an portfolio
+    Simple Portfolio class that has a collection of Stocks and a "Profit"
+    method that return the "annualized return" of the portfolio between the given dates.
     """
 
     def __init__(self, stocks):
-        """
-        Initialize the Portfolio with given stocks
-        """
         self.stocks = stocks
-
-    def add_one_year(self, date_edited):
-        """
-        Args:
-            start_date (datetime): The inital date.
-            end_date (datetime): The end date.
-
-        Return:
-            A date that's one years after the given date object 
-            if it exists, otherwise use the following day
-            (thus changing February 29 to February 28).
-
-        Ref: https://stackoverflow.com/questions/15741618/add-one-year-in-current-date
-        """
-        try:
-            return date_edited.replace(year = date_edited.year + 1)
-        except ValueError:
-            return date_edited.replace(year = date_edited.year + 1, day=28)
-
-    def rate_of_return(self, start_date, end_date):
-        """
-        Compute the rate of return given 2 dates .
-
-        Args:
-            start_date (datetime): The inital date.
-            end_date (datetime): The end date.
-
-        Returns:
-            float: The rate of return between the given dates.
-        """
-        value_start_date = sum([stock.price(start_date) for stock in self.stocks])
-        value_end_date = sum([stock.price(end_date) for stock in self.stocks])
-        return ((value_end_date - value_start_date ) / value_start_date)
     
     def profit(self, start_date, end_date):
         """
-        Formula ref: https://www.investopedia.com/terms/a/annualized-total-return.asp
+        Reference: https://www.investopedia.com/terms/a/annualized-total-return.asp
         """
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        tmp_date = self.add_one_year(start_date)
-        annual_rates = []
-        total_years = 0
+        days_between_dates = (end_date - start_date).days
+        if days_between_dates < 365:
+            """
+            From the previous link: 'calculating an annualized rate of return must
+            be based on historical numbers.'
+            """
+            raise ValueError("The two dates must be at least one year apart.") 
 
-        while tmp_date < end_date:
-            annual_rates.append(self.annual_rate(start_date, tmp_date))
-            start_date = tmp_date
-            tmp_date = self.add_one_year(tmp_date)
-            total_years = total_years + 1
+        started_value = sum([stock.price(start_date) for stock in self.stocks])
+        end_value = sum([stock.price(end_date) for stock in self.stocks])
+        cumulative_return = ((end_value / started_value ) - 1)
+        annualized_return = (1 + cumulative_return)**(365/days_between_dates) - 1
+        return annualized_return * 100
 
-        product_annual_rates = 1
-        for annual_rate in annual_rates:
-            product_annual_rates = product_annual_rates * (1 + annual_rate)
-        return ((product_annual_rates**(1/total_years)) - 1) * 100
-
-stocks = [Stock() for i in range(0, 10)]
+stocks = [Stock() for i in range(0, 1)]
 portfolio = Portfolio(stocks)
-print(portfolio.profit("2020-11-12", "2025-02-28"))
+
+# Test
+start_date = date(2020, 11, 1)
+end_date = date(2022, 5, 30)
+print("{} %".format(portfolio.profit(start_date, end_date)))
